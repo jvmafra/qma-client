3f3d8dea43b83f9d352ca18720f27a0998f4aa15
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2018-06-30 23:59:58 -0300

    adicionando tela de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes: Routes = [
+  { path: 'register', component: RegisterComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent {
+    model: any = {};
+    loading = false;
+
+    constructor() { }
+
+    register() {
+        console.log('Cliquei em registrar');
+    }
+}
